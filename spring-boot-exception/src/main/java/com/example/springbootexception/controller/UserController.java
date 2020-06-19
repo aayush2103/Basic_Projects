ee823e685a905d76fa2df7b8123a7e7990beb787
aayush2103 <54365067+aayush2103@users.noreply.github.com>
2020-06-20 01:25:06 +0530

    using @Exceptionhandler
    this enables to route a particular exception to a desired page

--- spring-boot-exception/src/main/java/com/example/springbootexception/controller/UserController.java
+++ spring-boot-exception/src/main/java/com/example/springbootexception/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.example.springbootexception.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class UserController {
@@ -9,5 +11,28 @@ public class UserController {
     public String add(){
         int num = 10/0;
         return "add";
+    }
+
+    @RequestMapping("update")
+    public String update(){
+        String name = null;
+        name = name.toLowerCase();
+        return "update";
+    }
+
+    @ExceptionHandler(value={java.lang.ArithmeticException.class})
+    public ModelAndView handlerArithmeticException(Exception e){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e.toString());
+        modelAndView.setViewName("mathError");
+        return modelAndView;
+    }
+
+    @ExceptionHandler(value={java.lang.NullPointerException.class})
+    public ModelAndView handlerNullPointerException(Exception e){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("exception", e.toString());
+        modelAndView.setViewName("nullPointerError");
+        return modelAndView;
     }
 }
