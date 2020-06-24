fdd9388a412f655a6b3deef48b91036d139ad789
aayush2103 <54365067+aayush2103@users.noreply.github.com>
2020-06-25 00:56:09 +0530

    Creating new spring-boot project for Chatroom

---
+++ chatroom-application/src/test/java/edu/udacity/java/nano/chatroomapplication/ChatroomApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.udacity.java.nano.chatroomapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
