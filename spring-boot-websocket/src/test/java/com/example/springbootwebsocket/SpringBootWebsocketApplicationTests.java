155b5cd7b2ed12caf9da3c3cb60f6ef5cda1233c
aayush2103 <54365067+aayush2103@users.noreply.github.com>
2020-06-25 00:36:09 +0530

    adding unit test to package jar

--- spring-boot-websocket/src/test/java/com/example/springbootwebsocket/SpringBootWebsocketApplicationTests.java
+++ spring-boot-websocket/src/test/java/com/example/springbootwebsocket/SpringBootWebsocketApplicationTests.java
@@ -1,13 +1,23 @@
 package com.example.springbootwebsocket;
 
+import com.example.springbootwebsocket.controller.UserController;
+import com.example.springbootwebsocket.model.User;
+import com.example.springbootwebsocket.model.UserResponse;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringBootWebsocketApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testUserResponse() {
+		UserController controller = new UserController();
+		UserResponse response = controller.getUser(new User("Aayush"));
+		Assert.assertEquals(response.getContent(), "Hello, Aayush");
 	}
 
 }
