ba1d4f9e681e990c7f8dc3b83150356ed3ab52e9
aayush2103 <54365067+aayush2103@users.noreply.github.com>
2020-08-21 02:56:47 +0530

    creating project spring-boot-bootstrap
    demo project for REST Api

---
+++ spring-boot-bootstrap/src/test/java/com/udacity/springbootbootstrap/SpringBootBootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.springbootbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
