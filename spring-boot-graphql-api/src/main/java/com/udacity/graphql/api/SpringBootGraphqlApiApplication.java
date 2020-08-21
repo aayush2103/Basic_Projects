2380c2f333dccdd93a42d9fce0d173132f3d0798
aayush2103 <54365067+aayush2103@users.noreply.github.com>
2020-08-22 03:18:45 +0530

    Creating new spring-boot-graphql-api project

---
+++ spring-boot-graphql-api/src/main/java/com/udacity/graphql/api/SpringBootGraphqlApiApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.graphql.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphqlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphqlApiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-graphql-api/src/test/java/com/udacity/graphql/api/SpringBootGraphqlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.graphql.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGraphqlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
