2e7fe77a3d7f495c29bbac913af61158413eb5b0
aayush2103 <54365067+aayush2103@users.noreply.github.com>
2020-10-30 02:10:50 +0530

    Created new project DogMicroservice

---
+++ DogMicroservice/src/main/java/com/udacity/dogMicroservice/repository/DogRepository.java
@@ -0,0 +1,7 @@
+package com.udacity.dogMicroservice.repository;
+
+import com.udacity.dogMicroservice.entity.Dog;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DogRepository extends CrudRepository<Dog, Long> {
+}
