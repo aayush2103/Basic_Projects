0f0be2dc1ae568152703821c704ce191b05df9ae
aayush2103 <54365067+aayush2103@users.noreply.github.com>
2020-10-31 01:33:31 +0530

    enable microservice client discovery

--- DogMicroservice/src/main/java/com/udacity/dogMicroservice/DogMicroserviceApplication.java
+++ DogMicroservice/src/main/java/com/udacity/dogMicroservice/DogMicroserviceApplication.java
@@ -2,8 +2,11 @@ package com.udacity.dogMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+/*to enable service discovery as a client on Eureka server*/
+@EnableEurekaClient
 public class DogMicroserviceApplication {
 
 	public static void main(String[] args) {
