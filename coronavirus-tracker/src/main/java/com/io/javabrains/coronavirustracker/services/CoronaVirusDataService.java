2692b9b596935a99d6c5c0cf6096e1c61e8670a1
aayush2103 <54365067+aayush2103@users.noreply.github.com>
2021-04-24 13:41:42 +0530

    csv file reader to parse data

--- coronavirus-tracker/src/main/java/com/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
+++ coronavirus-tracker/src/main/java/com/io/javabrains/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,9 +1,12 @@
 package com.io.javabrains.coronavirustracker.services;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -23,7 +26,14 @@ public class CoronaVirusDataService {
                 .build();
 
         HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-        System.out.println(httpResponse.body());
+        //System.out.println(httpResponse.body());
+
+        StringReader csvBodyReader = new StringReader(httpResponse.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
+        for(CSVRecord record: records){
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
     }
 
 }
