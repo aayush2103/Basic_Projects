d2a9409ad26da453ea421f16c10539de89c42216
aayush2103 <54365067+aayush2103@users.noreply.github.com>
2021-08-18 02:14:25 +0530

    prototype : demo2

---
+++ CreationalPatterns/src/main/java/com/pluralsight/org/prototype/Movie.java
@@ -0,0 +1,14 @@
+package com.pluralsight.org.prototype;
+
+public class Movie extends Item {
+
+    private String runtime;
+
+    public String getRuntime() {
+        return runtime;
+    }
+
+    public void setRuntime(String runtime) {
+        this.runtime = runtime;
+    }
+}
