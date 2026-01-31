3f0c40b78e90563c141308b1b5266fb901a88909
aayush2103 <54365067+aayush2103@users.noreply.github.com>
2026-01-31 13:37:04 +0530

    bridge demo4

---
+++ StructuralPatterns/src/main/java/com/pluralsight/org/bridge/demo3/BridgeDemo.java
@@ -0,0 +1,12 @@
+package com.pluralsight.org.bridge.demo3;
+
+public class BridgeDemo {
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setClassification("Action");
+        movie.setTitle("John Wick");
+        movie.setRuntime("2:15");
+        movie.setYear("2014");
+    }
+}
